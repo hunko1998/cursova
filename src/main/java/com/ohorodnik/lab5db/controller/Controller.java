9154f5b6afcd31e6fc549b5106de1dc32e92251b
hunko1998 <vitalihunko251617@gmail.com>
2018-05-31 11:30:25 +0300

    commit8

--- src/main/java/com/ohorodnik/lab5db/controller/Controller.java
+++ src/main/java/com/ohorodnik/lab5db/controller/Controller.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class Controller {
     @RequestMapping("/")
     public String getIndex() {
-        return "tables.html";
+        return "index.html";
     }
 }
