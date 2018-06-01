9e235f3268c47e9215472572d0b8eab9e9358c08
hunko1998 <vitalihunko251617@gmail.com>
2018-06-01 10:46:03 +0300

    commit11

--- src/main/java/com/ohorodnik/lab5db/repository/LibrarianRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ohorodnik.lab5db.repository;
-
-import com.ohorodnik.lab5db.model.Librarian;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LibrarianRepository extends JpaRepository<Librarian,Integer> {
-}
--- src/main/java/com/ohorodnik/lab5db/repository/StoragePlaceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ohorodnik.lab5db.repository;
-
-import com.ohorodnik.lab5db.model.StoragePlace;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StoragePlaceRepository extends JpaRepository<StoragePlace,Integer>{
-}
